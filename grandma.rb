600118af50af7d5f5e01af2c370679731c18f478
Adam Workman <adammworkman25@gmail.com>
2020-06-15 20:36:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-def uppercase
+
   if phrase == phrase.uppercase
     puts "NO, NOT SINCE 1938!"
   elsif phrase == "I LOVE YOU GRANDMA!"
